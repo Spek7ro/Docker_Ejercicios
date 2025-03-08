ff097716a89a64baa4cb9563141ad80ac2a7c08e
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-03-07 21:55:54 -0600

    add main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import logging, sys
+import time
+import requests
+
+h = [
+  logging.FileHandler('./log.log'), 
+  logging.StreamHandler(stream=sys.stdout),
+]
+
+logging.basicConfig(
+  level=logging.INFO,
+  format='%(asctime)s - %(levelname)s - %(message)s',
+  handlers=h,
+)
+
+logger = logging.getLogger(__name__)
+
 def main():
-  pass
+  r = requests.get('https://cristian-web.vercel.app/')
+  logger.info('Hello World!')
+  logger.info('Iniciando un ejercicio')
+  time.sleep(5)
+  logger.info(r.text)
+  logger.info('37')
+  logger.info('Terminando un ejercicio')
 
 
 if __name__ == '__main__':
